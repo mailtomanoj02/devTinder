12119c5d5d7f018c3805bbfd030f56266aab95a2
fh-manoj <manojs@FH-192.local>
2025-03-12 19:15:35 +0530

    Initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
 
   return (
     <>
-      
-      <h1>Dev Tinder</h1>
+     <h1 class="text-5xl font-bold underline">
+    Hello world!
+  </h1>
     </>
   )
 }
