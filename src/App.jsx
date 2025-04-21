b66c18afe47cba8c77ed471c35e9726c7325e197
Manoj S <manojs@FH-0170.local>
2025-04-21 21:34:27 +0530

    commit final

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-
+import { BrowserRouter, Routes, Route } from "react-router";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import { Provider } from "react-redux";
+import { store } from "./redux/Store";
+import Feed from "./components/Feed";
+import Connections from "./components/Connections";
+import Requests from "./components/Requests";
 
 function App() {
-
   return (
-    <>
-     <h1 class="text-5xl font-bold underline">
-    Hello world!
-  </h1>
-    </>
-  )
+    <Provider store={store}>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/" element={<Feed />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/connections" element={<Connections />} />
+            <Route path="/requests" element={<Requests />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
